936f2a21c50cdfc8506c0306814b6ab51c22fc69
AlejoBlom <alejandrogarzon197@gmail.com>
2024-04-26 15:05:51 -0500

    Mejoras en el formulario [FULL]

--- src/app/category/all/components/create/CategoryForm.tsx
+++ src/app/category/all/components/create/CategoryForm.tsx
@@ -1,59 +1,86 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import { Resolver, useForm } from 'react-hook-form'
-// import "../../css/create.product.form.css";
 import { defaultValues, IFormValuesC, schemaCategory } from './models'
+import { useCreateCategoryStore } from '../../../store/use.create.category.store';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const CategoryForm = () => {
   const {
     register,
+    handleSubmit,
     reset,
-    formState: { errors }
+    formState: { errors },
   } = useForm<IFormValuesC>({
     defaultValues,
-    resolver: yupResolver(schemaCategory) as Resolver<IFormValuesC, any>
-  })
+    resolver: yupResolver(schemaCategory) as Resolver<IFormValuesC, any>,
+  });
+
+  const navigate = useNavigate()
+  const { createCategory } = useCreateCategoryStore();
+  const [error, setError] = useState<string | null>(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit = async (data: IFormValuesC) => {
+    setIsSubmitting(true);
+    try {
+      await createCategory({
+        name: data.name,
+        image: data.image,
+      });
+      reset();
+      setError(null);
+    } catch (error) {
+      console.error("Error creating product:", error);
+      setError("No se ha podido crear la categoría");
+    } finally {
+      setIsSubmitting(false);
+    }
+    window.alert('¡Producto creado con éxito!');
+    navigate("/categories")
+
+  };
 
   return (
     <div>
+      {error && <div className="error">{error}</div>}
       <h2>Create Category</h2>
-
-      <form className='create-category-form'>
-        <div className='form-group'>
+      <form onSubmit={handleSubmit(onSubmit)} className="create-product-form">
+        <div className="form-group">
           <label>
-            <strong>Name:</strong>
+            <strong>Nombre:</strong>
           </label>
           <input
-            type='text'
-            {...register('name')}
-            placeholder='Nombre aquí'
-            className='large-input'
+            type="text"
+            {...register("name")}
+            placeholder="Titulo aquí"
+            className="large-input"
           />
           {errors.name && (
-            <strong className='error'>{errors.name.message}</strong>
+            <strong className="error">{errors.name.message}</strong>
           )}
         </div>
 
-        <div className='form-group'>
+        <div className="form-group">
           <label>
             <strong>Image:</strong>
           </label>
           <input
-            placeholder='Imagen aquí'
-            type='text'
-            {...register('image')}
-            className='large-input'
+            placeholder="Imagen aquí"
+            type="text"
+            {...register("image")}
+            className="large-input"
           />
           {errors.image && (
-            <strong className='error'>{errors.image.message}</strong>
+            <strong className="error">{errors.image.message}</strong>
           )}
         </div>
 
-        <button type='submit'>Create Category</button>
-        <button type='reset'>Reset Camps</button>
+        <button type="submit" disabled={isSubmitting}>Create Category</button>
       </form>
       <hr />
     </div>
-  )
+  );
 }
 
 export default CategoryForm
